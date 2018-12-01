5fb4c09571d9874002f4c1e0ac4b9cef9b31e844
cdbbinary <cdb.binary@gmail.com>
2018-12-01 00:35:42 -0500

    added CRUD methods and edited show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,22 @@
 class ArticlesController < ApplicationController
+# A frequent practice is to place the standard CRUD actions 
+# in each controller in the following order: index, show, new, 
+#edit, create, update and destroy.
+
+	def index
+
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
 
 	def new
+
+	end
+
+	def edit
+
 	end
 
 	def create
@@ -8,6 +24,14 @@ class ArticlesController < ApplicationController
 
 		@article.save
 		redirect_to @article
+	end
+
+	def update
+
+	end
+
+	def destroy
+
 	end
 
 	private
