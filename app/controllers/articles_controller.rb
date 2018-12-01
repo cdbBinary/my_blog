0b3f4e871b0d7572c709edbaae60dce8951b52d9
cdbbinary <cdb.binary@gmail.com>
2018-11-30 23:23:01 -0500

    created controller and model, and deleted some files

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,13 @@
 class ArticlesController < ApplicationController
 
-def new
-end
+	def new
+	end
+
+	def create
+		@article = Article.new(params.require(:article).permit(:title, :text))
+
+		@article.save
+		redirect_to @article
+	end
 
 end
